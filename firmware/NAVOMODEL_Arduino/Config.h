e0c7672deb54a9a8db81dab29fe87d9363423d81
apreuteseicostel <65179130+apreuteseicostel@users.noreply.github.com>
2026-09-18 08:18:11 +0100

    Add Rev.D firmware configuration

---
+++ firmware/NAVOMODEL_Arduino/Config.h
@@ -0,0 +1,11 @@
+#pragma once
+#define HOLD_MS 180UL
+#define RC_REVERSE 0
+#define ADC_REF_MV 5000UL
+#define RGBW_PIXEL 0
+#define ENABLE_WATCHDOG 0
+#define H743_TELEMETRY 1
+#define H743_BAUD 57600UL
+#define H743_PERIOD_MS 500UL
+// D0/D1 are dedicated to H743 UART when telemetry is enabled.
+#define DIAGNOSTICS 0
