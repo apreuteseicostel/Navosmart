5c92643c93921581874bd6484674a74b4ad51b92
apreuteseicostel <65179130+apreuteseicostel@users.noreply.github.com>
2026-09-18 16:34:09 +0100

    Expose Kogger TCP transport to NAVO SMART QML

--- custom/src/CustomPlugin.cc
+++ custom/src/CustomPlugin.cc
@@ -1,11 +1,11 @@
 #include "CustomPlugin.h"
-#include "NavoKoggerDecoder.h"
+#include "NavoKoggerDecoder.h"\n#include "NavoKoggerTcpClient.h"
 #include <QtQml/qqml.h>
 #include <QtQml/QQmlApplicationEngine>
 #include <QtCore/QFile>
 CustomFlyViewOptions::CustomFlyViewOptions(CustomOptions* options,QObject* parent):QGCFlyViewOptions(options,parent){}
 CustomPlugin::CustomPlugin(QObject* parent):QGCCorePlugin(parent),_options(new CustomOptions(this)){
- qmlRegisterType<NavoKoggerDecoder>("NavoSmart.Backend",1,0,"NavoKoggerDecoder");
+ qmlRegisterType<NavoKoggerDecoder>("NavoSmart.Backend",1,0,"NavoKoggerDecoder");\n qmlRegisterType<NavoKoggerTcpClient>("NavoSmart.Backend",1,0,"NavoKoggerTcpClient");
 }
 CustomPlugin::~CustomPlugin(){}
 QQmlApplicationEngine* CustomPlugin::createQmlApplicationEngine(QObject* parent){
