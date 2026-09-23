039f7f36fc9f783863a87bc03c4ce07b7e457fd0
apreuteseicostel <65179130+apreuteseicostel@users.noreply.github.com>
2026-09-23 10:18:16 +0100

    Add bathymetry coordinate and mesh engine

---
+++ custom/src/NavoBathymetryMesh.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <QObject>
+#include <QVariantList>
+#include <QVector3D>
+
+class NavoBathymetryMesh : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(QVariantList vertices READ vertices NOTIFY meshChanged)
+    Q_PROPERTY(QVariantList triangles READ triangles NOTIFY meshChanged)
+    Q_PROPERTY(double minDepthM READ minDepthM NOTIFY meshChanged)
+    Q_PROPERTY(double maxDepthM READ maxDepthM NOTIFY meshChanged)
+    Q_PROPERTY(double originLatitude READ originLatitude NOTIFY meshChanged)
+    Q_PROPERTY(double originLongitude READ originLongitude NOTIFY meshChanged)
+public:
+    explicit NavoBathymetryMesh(QObject* parent=nullptr);
+    Q_INVOKABLE bool build(const QVariantList& samples, double gridSizeM=2.0, double maxGapM=6.0);
+    Q_INVOKABLE void clear();
+    QVariantList vertices() const { return _vertices; }
+    QVariantList triangles() const { return _triangles; }
+    double minDepthM() const { return _minDepth; }
+    double maxDepthM() const { return _maxDepth; }
+    double originLatitude() const { return _originLat; }
+    double originLongitude() const { return _originLon; }
+signals: void meshChanged();
+private:
+    QVariantList _vertices, _triangles;
+    double _minDepth=0, _maxDepth=0, _originLat=0, _originLon=0;
+};
