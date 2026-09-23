ec98579970b365aad03675d71b8b6d602be196a4
apreuteseicostel <65179130+apreuteseicostel@users.noreply.github.com>
2026-09-23 12:29:07 +0100

    Fix automoc detection for NavoTrack3DGeometry

--- custom/src/NavoTrack3DGeometry.h
+++ custom/src/NavoTrack3DGeometry.h
@@ -1,6 +1,29 @@
 #pragma once
+
 #include <QQuick3DGeometry>
 #include <QVariantList>
-class NavoTrack3DGeometry:public QQuick3DGeometry{Q_OBJECT Q_PROPERTY(QVariantList points READ points WRITE setPoints NOTIFY changed) Q_PROPERTY(float width READ width WRITE setWidth NOTIFY changed)
-public:explicit NavoTrack3DGeometry(QQuick3DObject*p=nullptr);QVariantList points()const{return _p;}float width()const{return _w;}void setPoints(const QVariantList&);void setWidth(float);
-signals:void changed();private:void rebuild();QVariantList _p;float _w=.32f;};
+
+class NavoTrack3DGeometry : public QQuick3DGeometry
+{
+    Q_OBJECT
+    Q_PROPERTY(QVariantList points READ points WRITE setPoints NOTIFY changed)
+    Q_PROPERTY(float width READ width WRITE setWidth NOTIFY changed)
+
+public:
+    explicit NavoTrack3DGeometry(QQuick3DObject* parent = nullptr);
+
+    QVariantList points() const { return _p; }
+    float width() const { return _w; }
+
+    void setPoints(const QVariantList& points);
+    void setWidth(float width);
+
+signals:
+    void changed();
+
+private:
+    void rebuild();
+
+    QVariantList _p;
+    float _w = .32f;
+};
